8b3cd9aa31f45613d069d396a0cb455ea97801ce
Alvaro <alvarocastro74@gmail.com>
2018-09-20 23:09:20 -0600

    Register company done

--- FrontEnd/src/app/general/create-company/create-company.component.ts
+++ FrontEnd/src/app/general/create-company/create-company.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-create-company',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateCompanyComponent implements OnInit {
 
+  formCompany: FormGroup;
+
   constructor() { }
 
   ngOnInit() {
-  }
+    this.formCompany = new FormGroup({
+      'cedulaJuridica' : new FormControl(null, [Validators.min(10000000),
+        Validators.max(9999999999), Validators.required]),
+      'nombreEmpresa' : new FormControl(null,Validators.required)
+    })
 
 }
