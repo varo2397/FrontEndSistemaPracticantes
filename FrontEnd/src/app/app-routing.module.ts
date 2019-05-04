8ad0135ab318c81a2840c2a05e81b9fbf8a645b0
varo2397 <alvarocastro74@gmail.com>
2019-05-04 16:48:05 -0600

    guard for not authenticated users

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './core/routing/auth-guard.service';
 
 const routes: Routes = [
   { path: '', loadChildren: './auth/auth.module#AuthModule' },
-  { path: 'coordinador', loadChildren: './coordinator/coordinator.module#CoordinatorModule' },
-  { path: 'empresa', loadChildren: './company/company.module#CompanyModule' },
-  { path: 'estudiante', loadChildren: './student/student.module#StudentModule' }
+  { path: 'coordinador', loadChildren: './coordinator/coordinator.module#CoordinatorModule', canLoad: [AuthGuard] },
+  { path: 'empresa', loadChildren: './company/company.module#CompanyModule', canLoad: [AuthGuard] },
+  { path: 'estudiante', loadChildren: './student/student.module#StudentModule', canLoad: [AuthGuard]  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
---
+++ FrontEnd/src/app/core/routing/auth-guard.service.ts
@@ -0,0 +1,40 @@
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot } from '@angular/router';
+import { Store, select } from '@ngrx/store';
+import * as fromRoot from '../../store/app.reducer';
+import { from, Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { map, take } from 'rxjs/operators';
+
+@Injectable()
+export class AuthGuard implements CanActivate, CanLoad {
+
+  constructor(private store: Store<fromRoot.State>, private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot, routerState: RouterStateSnapshot): Observable<boolean> {
+    return this.store.pipe(
+      select(fromRoot.getIsAuthenticated),
+      map(isAuthenticated => {
+        console.log(isAuthenticated)
+        if (!isAuthenticated) {
+          this.router.navigate(['/']);
+          return false;
+        }
+
+        return true;
+      }),
+      take(1)
+    );
+  }
+
+  canLoad(route: Route): Observable<boolean> | boolean {
+    return this.store.select(fromRoot.getIsAuthenticated).pipe(map((isAuthenticated) => {
+      if (isAuthenticated) {
+        return true;
+      }
+      else {
+        this.router.navigate(['/']);
+        return false;
+      }
+    }));
+  }
+}
