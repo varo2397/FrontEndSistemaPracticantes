e7a054861a76b4f8252365f0ebfbc2d31975c23f
varo2397 <alvarocastro74@gmail.com>
2019-05-04 15:48:56 -0600

    Activity schedule ready

--- FrontEnd/src/app/company/events/event/event.component.ts
+++ FrontEnd/src/app/company/events/event/event.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  TemplateRef,
+  AfterViewInit,
+  ViewContainerRef,
+  OnDestroy,
+  OnInit
+} from '@angular/core';
+import {Overlay, OverlayRef} from '@angular/cdk/overlay';
+import {TemplatePortal} from '@angular/cdk/portal';
 import {ActivatedRoute} from '@angular/router';
 
 @Component({
@@ -6,11 +16,17 @@ import {ActivatedRoute} from '@angular/router';
   templateUrl: './event.component.html',
   styleUrls: ['./event.component.scss']
 })
-export class EventComponent implements OnInit {
+export class EventComponent implements OnInit, AfterViewInit, OnDestroy {
 
   talkEvent: boolean;
   eventID: number;
-  constructor(private route: ActivatedRoute) { }
+  @ViewChild(TemplateRef) _dialogTemplate: TemplateRef<any>;
+  private _overlayRef: OverlayRef;
+  private _portal: TemplatePortal;
+
+  constructor(private _overlay: Overlay,
+              private _viewContainerRef: ViewContainerRef,
+              private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.talkEvent = false;
@@ -21,6 +37,23 @@ export class EventComponent implements OnInit {
 
   showTalkEvent() {
     this.talkEvent = !this.talkEvent;
+  }
+
+  ngAfterViewInit() {
+    this._portal = new TemplatePortal(this._dialogTemplate, this._viewContainerRef);
+    this._overlayRef = this._overlay.create({
+      positionStrategy: this._overlay.position().global().centerHorizontally().centerVertically(),
+      hasBackdrop: true
+    });
+    this._overlayRef.backdropClick().subscribe(() => this._overlayRef.detach());
+  }
+
+  ngOnDestroy() {
+    this._overlayRef.dispose();
+  }
+
+  openDialog() {
+    this._overlayRef.attach(this._portal);
   }
 
 }
