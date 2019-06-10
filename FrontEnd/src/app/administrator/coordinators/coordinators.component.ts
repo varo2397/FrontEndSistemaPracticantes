41f0fb68a5df135f7012d15c4818d2ef8dd3f2e9
varo2397 <alvarocastro74@gmail.com>
2019-06-10 00:13:13 -0600

    Coordinators UI ready

--- FrontEnd/src/app/administrator/coordinators/coordinators.component.ts
+++ FrontEnd/src/app/administrator/coordinators/coordinators.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort, MatTableDataSource } from '@angular/material';
+import { Coordinator } from '../../interfaces/coordinator';
+
 
 @Component({
   selector: 'app-coordinators',
   templateUrl: './coordinators.component.html',
   styleUrls: ['./coordinators.component.scss']
 })
+
 export class CoordinatorsComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = [
+    'name',
+    'lastName',
+    'secondLastName',
+    'gender',
+    'email',
+    'actions'
+  ];
+  events: Event[];
+  dataSource: MatTableDataSource<Coordinator>; // new MatTableDataSource(this.events);
+  @ViewChild(MatSort) sort: MatSort;
 
   ngOnInit() {
+    this.getCoordinators();
+  }
+
+  getCoordinators() {
+    this.dataSource = new MatTableDataSource([]);
+    this.dataSource.sort = this.sort;
   }
 
 }
---
+++ FrontEnd/src/app/interfaces/coordinator.ts
@@ -0,0 +1,8 @@
+export interface Coordinator {
+  id?: number;
+  name: string;
+  lastName: string;
+  secondLastName: string;
+  gender: string;
+  email: string;
+}
