1617c49980f0b7991d90cc8f610ed244f48fb38b
varo2397 <alvarocastro74@gmail.com>
2019-06-12 13:49:18 -0600

    Assign teacher to student

--- FrontEnd/src/app/coordinator/students/select-student/select-student.component.ts
+++ FrontEnd/src/app/coordinator/students/select-student/select-student.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { StudentsService } from '../../../core/http/students/students.service';
+import { TeacherService } from '../../../core/http/teacher/teacher.service';
+import { Teacher } from '../../../interfaces/teacher';
 
 @Component({
   selector: 'app-select-student',
@@ -12,18 +14,29 @@ export class SelectStudentComponent implements OnInit {
   studentID: string;
 
   student: any;
+  teachers: [];
 
   constructor(private route: ActivatedRoute,
-              private studentsService: StudentsService) { }
+              private studentsService: StudentsService,
+              private teachersService: TeacherService) { }
 
   ngOnInit() {
     this.studentID = this.route.snapshot.paramMap.get('id');
+    this.getTeachers();
     this.getStudent();
   }
 
   getStudent() {
     this.studentsService.getStudent(this.studentID).subscribe(response => {
       this.student = response.data;
+    });
+  }
+
+  getTeachers() {
+    this.teachersService.getTeachers().subscribe(response => {
+      this.teachers = response.data.map(value => {
+        return value.name + ' ' + value.lastName;
+      });
     });
   }
 
